caf9ff6b2cf1fec6250afd58b408663471af8f5a
Devatoria <joris.bonnefoy@gmail.com>
2015-05-26 22:56:23 +0200

    manage args

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	args := os.Args[1:]
+	
+	if len(args) > 0 {
+		switch args[0] {
+		default:
+			fmt.Println("Wrong command name given")
+		}
+	} else {
+		fmt.Println("No command name given")
+	}
 }
