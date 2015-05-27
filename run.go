1a332b448abb2480fca292509f4964a34a0b6856
Devatoria <joris.bonnefoy@gmail.com>
2015-05-27 18:05:14 +0200

    add run command

--- run.go
+++ run.go
@@ -1,7 +1,14 @@
 package main
 
+import "github.com/GoGoupil/http"
+
 var runCommand = &Command {
 	Usage: "run",
 	ShortDesc: "Run goupil",
 	LongDesc: "TODO",
+	Run: run,
+}
+
+func run(args []string) {
+	
 }
