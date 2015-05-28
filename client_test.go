c21409c90f8f7d17ef67a8ee3c2763ca33ec369b
Devatoria <joris.bonnefoy@gmail.com>
2015-05-29 00:59:26 +0200

    Enhance test to launch ten threads

--- client_test.go
+++ client_test.go
@@ -2,15 +2,25 @@ package http
 
 import (
 	"github.com/GoGoupil/assert"
+	"sync"
 	"testing"
 )
 
 func TestReturnCode(t *testing.T) {
 	var client = Client{
-		BaseURL: "http://www.google.fr",
+		BaseURL: "http://devatoria.info",
 	}
 
-	code := client.Get("/")
+	wg := sync.WaitGroup{}
 
-	assert.AssertEqual(t, code, 200)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			code := client.Get("/")
+			assert.AssertEqual(t, code, 200)
+		}(i)
+	}
+
+	wg.Wait()
 }
