148fdbbab48795c083e03706c623a83c57f64794
Devatoria <joris.bonnefoy@gmail.com>
2015-05-29 09:35:56 +0200

    Review run command help message

--- run.go
+++ run.go
@@ -1,9 +1,26 @@
 package main
 
 var runCommand = &Command{
-	Usage:     "run",
-	ShortDesc: "Run goupil",
-	LongDesc:  "TODO",
+	Usage:     "run plan.json",
+	ShortDesc: "Execute given plan",
+	LongDesc:  `The plan parameter is a JSON filed describing the plan to execute.
+Here is an example of plan description file.
+You can check Plan structure defined in plan.go or check our GitHub to have more details :)
+
+{
+	"Name": "test",
+	"BaseURL": "http://www.google.com",
+	"Threads": [
+		{
+			"Count": 10,
+			"Route": "/"
+		},
+		{
+			"Count": 10,
+			"Route": "/test"
+		}
+	]
+}`,
 	MinArgs:   1,
 	Run:       run,
 }
